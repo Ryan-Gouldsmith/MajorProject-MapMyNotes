e9492dba1434341513683280279061c428da53d6
RyanGouldsmith <GouldsmithRyan@hotmail.co.uk>
2016-02-17 20:48:11 +0000

    Reads in the image now

--- tesseract_training_data/testing_tesseract_performance/test_training_blue/scripts/blueline_adaptive_threshold.py
+++ tesseract_training_data/testing_tesseract_performance/test_training_blue/scripts/blueline_adaptive_threshold.py
@@ -1,13 +1,34 @@
 #!/usr/bin/python
 
 import os
+import cv2
+import numpy as np
+
 
 class Blueline_Adaptive_Threshold(object):
+
+    GRAYSCALE_VALUE = 0
 
     def image_file_exists(self, filename):
         # Going up a directory http://stackoverflow.com/questions/9856683/using-pythons-os-path-how-do-i-go-up-one-directory
         script_root = os.path.dirname(os.path.dirname(__file__))
         return os.path.isfile(os.path.join(script_root, filename))
+
+
+    def read_image(self, filename):
+        if self.image_file_exists(filename):
+            self.grayscale_image = cv2.imread(filename)
+        else:
+            return None
+
+        return self.grayscale_image
+
+
+
+
+
+
+
 
 
 
--- tesseract_training_data/testing_tesseract_performance/test_training_blue/test_script/blue_lined_adaptive_threshold_test.py
+++ tesseract_training_data/testing_tesseract_performance/test_training_blue/test_script/blue_lined_adaptive_threshold_test.py
@@ -10,11 +10,23 @@ import test_training_blue.scripts.blueline_adaptive_threshold as blueline
 class TestBlueLinedAdaptedThreshold(object):
     def setup(self):
         self.threshold = blueline.Blueline_Adaptive_Threshold()
-        self.file_exists = "test_script/test_image.jpg"
+        self.image_file = "test_script/test_image.jpg"
 
     def test_image_exists(self):
-        assert True == self.threshold.image_file_exists(self.file_exists)
+        assert True == self.threshold.image_file_exists(self.image_file)
 
     def test_image_doesnot_exist(self):
         false_file = "/test_script/false_image.jpg"
         assert False == self.threshold.image_file_exists(false_file)
+
+    def test_reading_image(self):
+        assert None is not self.threshold.read_image(self.image_file)
+
+    def test_grayscale_is_an_array(self):
+        grayscale_image = self.threshold.read_image(self.image_file)
+
+        assert len(grayscale_image) is not 0
+
+    def test_convert_to_grayscale(self):
+
+        
