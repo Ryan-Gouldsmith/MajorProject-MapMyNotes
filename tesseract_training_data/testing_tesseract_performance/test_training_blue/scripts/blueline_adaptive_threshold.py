a93d193ef070fe722906b999d2681284e5b4beb6
RyanGouldsmith <GouldsmithRyan@hotmail.co.uk>
2016-02-17 21:56:30 +0000

    Added a lot of the eroding methods

--- tesseract_training_data/testing_tesseract_performance/test_training_blue/scripts/blueline_adaptive_threshold.py
+++ tesseract_training_data/testing_tesseract_performance/test_training_blue/scripts/blueline_adaptive_threshold.py
@@ -3,29 +3,38 @@
 import os
 import cv2
 import numpy as np
+import sys
 
 
 class Blueline_Adaptive_Threshold(object):
 
     GRAYSCALE_VALUE = 0
+    SMALL_KERNEL_SIZE = 2
+    ERODE_ITERATION = 1
 
     def image_file_exists(self, filename):
         # Going up a directory http://stackoverflow.com/questions/9856683/using-pythons-os-path-how-do-i-go-up-one-directory
         script_root = os.path.dirname(os.path.dirname(__file__))
         return os.path.isfile(os.path.join(script_root, filename))
 
-
     def read_image(self, filename):
         if self.image_file_exists(filename):
-            self.grayscale_image = cv2.imread(filename)
+            self.image = cv2.imread(filename)
         else:
             return None
 
+        return self.image
+
+    def convert_to_grayscale(self, image):
+        self.grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
         return self.grayscale_image
 
+    def get_kernel(self, kernel_size):
+        return np.ones((kernel_size,kernel_size),np.uint8)
 
-
-
+    def erode_image(self, greyscale_image, kernel_size):
+        eroded_image = cv2.erode(greyscale_image,kernel_size,iterations = self.ERODE_ITERATION)
+        return eroded_image
 
 
 
@@ -36,9 +45,18 @@ class Blueline_Adaptive_Threshold(object):
 
 if __name__ == "__main__":
 
-    true_or_false = image_file_exists("foo")
+    # Read in an image as colour
+    image = read_image(sys.argv[1])
 
-# Take an image from the input and convert it to grayscale
+    # Take an image from the input and convert it to grayscale
+    grayscale_image = convert_to_grayscale(image)
+
+    # Clear up the image by eroding.
+
+
+
+
+
 
 # perform smoothing and run threshold
 
--- tesseract_training_data/testing_tesseract_performance/test_training_blue/test_script/blue_lined_adaptive_threshold_test.py
+++ tesseract_training_data/testing_tesseract_performance/test_training_blue/test_script/blue_lined_adaptive_threshold_test.py
@@ -2,8 +2,9 @@
 import unittest
 import sys
 import pytest
-
+import cv2
 import os
+import numpy as np
 
 import test_training_blue.scripts.blueline_adaptive_threshold as blueline
 
@@ -13,20 +14,37 @@ class TestBlueLinedAdaptedThreshold(object):
         self.image_file = "test_script/test_image.jpg"
 
     def test_image_exists(self):
-        assert True == self.threshold.image_file_exists(self.image_file)
+        assert self.threshold.image_file_exists(self.image_file) is True
 
     def test_image_doesnot_exist(self):
         false_file = "/test_script/false_image.jpg"
-        assert False == self.threshold.image_file_exists(false_file)
+
+        assert self.threshold.image_file_exists(false_file) is False
 
     def test_reading_image(self):
-        assert None is not self.threshold.read_image(self.image_file)
+        assert self.threshold.read_image(self.image_file) is not None
 
     def test_grayscale_is_an_array(self):
-        grayscale_image = self.threshold.read_image(self.image_file)
+        coloured_image = self.threshold.read_image(self.image_file)
 
-        assert len(grayscale_image) is not 0
+        assert len(coloured_image) is not 0
 
     def test_convert_to_grayscale(self):
+        image = cv2.imread(self.image_file)
+        grayscale_image = self.threshold.convert_to_grayscale(image)
 
-        
+        assert grayscale_image is not None
+
+    def test_get_kernel_type(self):
+        kernel = self.threshold.get_kernel(2)
+
+        assert isinstance(kernel, np.ndarray) is True
+
+    def test_erroding_image(self):
+        image = cv2.imread(self.image_file)
+        greyscale_image = self.threshold.convert_to_grayscale(image)
+        kernel = self.threshold.get_kernel(2)
+
+        eroding_image = self.threshold.erode_image(greyscale_image, kernel)
+
+        assert eroding_image is not None
