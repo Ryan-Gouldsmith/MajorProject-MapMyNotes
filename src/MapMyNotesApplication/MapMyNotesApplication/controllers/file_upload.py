339f8f6d2d215527f7583ed354128d900a61b93d
RyanGouldsmith <GouldsmithRyan@hotmail.co.uk>
2016-02-19 17:39:23 +0000

    Added the tests for the routes which were needed to serve the Image, I am not overally happy we will see on performance

--- src/MapMyNotesApplication/MapMyNotesApplication/controllers/file_upload.py
+++ src/MapMyNotesApplication/MapMyNotesApplication/controllers/file_upload.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, send_from_directory, send_file, safe_join
 from werkzeug import secure_filename
 from MapMyNotesApplication.models.file_upload_service import FileUploadService
+import os
 
 fileupload = Blueprint('fileupload', __name__)
 
@@ -29,6 +30,26 @@ def file_upload_index_route():
 
     return render_template('/file_upload/index.html')
 
+
 @fileupload.route("/upload/show_note/<note_image>", methods=["GET"])
 def show_note(note_image):
-    return "testing"
+    file_exists = os.path.isfile('MapMyNotesApplication/upload/'+note_image)
+    if not file_exists:
+        return redirect(url_for('fileupload.error_four_zero_four'))
+
+    return render_template("/file_upload/show_note.html",note_image=note_image)
+
+
+# Not happy with this function, I think it will need to be looked into further down the down. Surely there's a better way than this. For some reason the send from directory did not work.... here https://github.com/mitsuhiko/flask/issues/1169
+@fileupload.route("/img/<path:note_image>")
+def get_image(note_image):
+    file_exists = os.path.isfile('MapMyNotesApplication/upload/'+note_image)
+    filename = secure_filename(note_image)
+    # TODO look for better way to fix this bug mentioned in the github issues
+    application_root = os.path.dirname(fileupload.root_path)
+    return send_from_directory(os.path.join(application_root, 'upload'), filename)
+
+
+@fileupload.route("/error/404")
+def error_four_zero_four():
+    return render_template("/error/404.html")
