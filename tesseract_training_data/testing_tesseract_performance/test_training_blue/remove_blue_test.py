0c4ac7e772af0e4beec51fbf88f7d9ab56fbf99a
RyanGouldsmith <GouldsmithRyan@hotmail.co.uk>
2016-02-22 17:35:13 +0000

    created a small script which removed the lines and noise around the image

--- tesseract_training_data/testing_tesseract_performance/test_training_blue/remove_blue_test.py
+++ tesseract_training_data/testing_tesseract_performance/test_training_blue/remove_blue_test.py
@@ -16,12 +16,14 @@ upper_blue = np.array([130,255,255])
 lower_black = np.array([0,0,0])
 upper_black = np.array([175,20, 95])
 
-kernel = np.ones((2,2),np.uint8)
+kernel = np.ones((3,3),np.uint8)
 
 
 
 # OpenCV library tutorials
 hsv = cv2.erode(hsv,kernel,iterations = 1)
+
+#hsv = cv2.adaptiveThreshold(hsv, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 1)
 
 #OpenCV library tutorials
 mask_black = cv2.inRange(hsv, lower_black, upper_black)
@@ -36,31 +38,36 @@ mask[np.where(mask_black == 0)] = 255
 
 kernel = np.ones((1,1),np.uint8)
 
-#mask = cv2.erode(mask,kernel,iterations = 1)
+mask = cv2.dilate(mask,kernel,iterations = 1)
 
-mask = cv2.medianBlur(mask,3,0)
+thresh7 = cv2.medianBlur(mask,3,0)
 
-thresh7 = cv2.adaptiveThreshold(mask, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 1)
+#
+
+#ret3,thresh7 = cv2.threshold(mask,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+
+
 
 
 
 
 
 #kernel = np.ones((1,1),np.uint8)
-kernel = np.ones((7,7),np.uint8)
+#kernel = np.ones((9,9),np.uint8)
 
-thresh7 = cv2.erode(thresh7,kernel,iterations = 1)
+#thresh7 = cv2.erode(thresh7,kernel,iterations = 1)
 
 #kernel = np.ones((4,4),np.uint8)
-kernel = np.ones((2,2),np.uint8)
+#kernel = np.ones((1,1),np.uint8)
 
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
 
-thresh7 = cv2.morphologyEx(thresh7, cv2.MORPH_CLOSE, kernel)
+thresh7 = cv2.morphologyEx(thresh7, cv2.MORPH_OPEN, kernel)
 
 image_path = "test_new_blue.tiff"
 
 cv2.imwrite(image_path, thresh7)
-
 
 cv2.imshow("im", thresh7)
 cv2.waitKey(0)
