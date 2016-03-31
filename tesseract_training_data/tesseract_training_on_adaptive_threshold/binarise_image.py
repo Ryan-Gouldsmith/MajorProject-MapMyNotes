55eedfbce095278550c188b9bed309e2e706760f
RyanGouldsmith <GouldsmithRyan@hotmail.co.uk>
2016-03-31 09:54:24 +0100

    referenced the material properly in the new script

--- tesseract_training_data/tesseract_training_on_adaptive_threshold/binarise_image.py
+++ tesseract_training_data/tesseract_training_on_adaptive_threshold/binarise_image.py
@@ -4,6 +4,11 @@ import copy
 import sys
 import os
 
+"""
+REFERENCE Ideas considered from, but changed it to match the lines of my own text  http://stackoverflow.com/questions/10196198/how-to-remove-convexity-defects-in-a-sudoku-square
+
+Excellent resource for extracting lines from music notes. Have been using as a template and how to consider my own line extraction. http://docs.opencv.org/trunk/d1/dee/tutorial_moprh_lines_detection.html#gsc.tab=0
+"""
 class BinariseImage(object):
 
     GRAYSCALE_INPUT = 0
@@ -42,6 +47,9 @@ class BinariseImage(object):
         return cv2.dilate(image,kernel_size,iterations = self.KERNEL_ITERATIONS)
 
     def create_empty_mask(self, row, column):
+        """
+        Reference material http://stackoverflow.com/questions/16235955/create-a-multichannel-zeros-mat-in-python-with-cv2
+        """
         return np.zeros((row,column),np.uint8)
 
     def convert_black_threshold_to_white(self, image):
@@ -49,6 +57,9 @@ class BinariseImage(object):
         return self.threshold_image
 
     def convert_white_to_black(self, image, blank_mask):
+        """
+         Found a better way to itterate over the image http://stackoverflow.com/questions/30331944/finding-red-color-using-python-opencv
+        """
         blank_mask[np.where(self.threshold_image == 0)] = 255
         return blank_mask
 
@@ -56,6 +67,9 @@ class BinariseImage(object):
         return np.ones((height,width),np.uint8)
 
     def find_contours_in_mask(self, mask):
+        """
+        About unpacking the variables some issue it didn't work some times http://stackoverflow.com/questions/25504964/opencv-python-valueerror-too-many-values-to-unpack
+        """
         contour_info = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         self.contours = contour_info[1]
         return contour_info
